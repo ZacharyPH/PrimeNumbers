1890bd514a2e880a92a4bf951d369caaf9c9ef69
Zachary Hunt <=>
2020-06-28 03:44:52 -0500

    Support for the PrimesList Python package

---
+++ HDF.py
@@ -0,0 +1,36 @@
+import os
+import h5py
+import numpy as np
+
+
+def init_hdf():
+    newpath = "./HDF/"
+    with h5py.File(newpath + "Primes" + ".hdf5", "w") as hdf:
+        hdf.create_dataset("Primes", (50 * 1000 * 1000,), data=np.zeros(50 * 1000 * 1000), dtype="i", chunks=True)
+
+
+def fill_hdf():
+    d = h5py.File("./HDF/" + "Primes" + ".hdf5", "r+")
+    path = "./Millions/"
+    for (_, _, filenames) in os.walk(path):
+        for i, file in enumerate(filenames):
+            print(file, end="\r")
+            with open(path + file, "r") as f:
+                d["Primes"][i * 1000 * 1000: (i + 1) * 1000 * 1000, ] = np.array([int(p) for p in f.read().split(",")])
+    return d
+
+
+def open_hdf():
+    return h5py.File("./HDF/" + "Primes" + ".hdf5", "r")
+
+
+def main():
+    # init_hdf()
+    # d = fill_hdf()
+    d = open_hdf()
+    d["Primes"][40 * 1000 * 1000 + 15: 40 * 1000 * 1000 + 28]
+
+
+if __name__ == "__main__":
+    main()
+# TODO: Sounds like I might need a Virtual Dataset
